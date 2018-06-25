d1ed64851928ac7d9a6c8397143697124d4f66bc
seavty <seavty.oeung@gmail.com>
2018-06-25 18:31:44 +0700

    finsh delete document api

---
+++ X-Admin-API/Controllers/DocumentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using X_Admin_API.Repository.Repo;
+
+namespace X_Admin_API.Controllers
+{
+    public class DocumentController : ApiController
+    {
+        private const string route = Helper.Helper.apiVersion + "documents";
+        private const string routeWithConstraint = route + "/{id:int:min(1)}";
+        private DocumentRepository repository = null;
+
+        public DocumentController()
+        {
+            repository = new DocumentRepository();
+        }
+
+        //-> Delete Document
+        [Route(routeWithConstraint)]
+        [HttpDelete]
+        public async Task<IHttpActionResult> Delete(int id)
+        {
+            try
+            {
+                if (await repository.Delete(id))
+                    return Ok();
+                return NotFound();
+            }
+            catch (HttpException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ X-Admin-API/Repository/Repo/DocumentRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using X_Admin_API.Models.DB;
+
+namespace X_Admin_API.Repository.Repo
+{
+    public class DocumentRepository
+    {
+        private THEntities db = null;
+
+        public DocumentRepository()
+        {
+            db = new THEntities();
+        }
+
+        //-> Delete
+        public async Task<Boolean> Delete(int id)
+        {
+            var record = await db.sm_doc.FirstOrDefaultAsync(x => x.docs_Deleted == null && x.id == id);
+            if (record == null)
+                throw new HttpException((int)HttpStatusCode.NotFound, "NotFound");
+            record.docs_Deleted = "1";
+            await db.SaveChangesAsync();
+            return true;
+        }
+    }
+}
